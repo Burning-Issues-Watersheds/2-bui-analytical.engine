e0d3d5d776ee0d60e546f4eede3dacc567401989
Francisco J. Guerrero <guerrero.francisco.jose@gmail.com>
2023-03-13 09:49:03 -0700

    update for the credential verfication script

--- script_git_credentials_authentication.R
+++ script_git_credentials_authentication.R
@@ -21,3 +21,26 @@ usethis::create_github_token()
 gitcreds::gitcreds_set() 
 usethis::git_sitrep()
 usethis::git_credentials_forget()
+
+#Source: https://nceas.github.io/scicomp-workshop-collaborative-coding/#github
+
+# Install the `usethis` and `gitcreds` packages
+#install.packages(c("usethis", "gitcreds"))
+library(usethis)
+library(gitcreds)
+
+# Add your GitHub username and email
+usethis::use_git_config(user.name = "Francisco J. Guerrero",
+                        user.email = "guerrero.francisco.jose@gmail.com")
+
+# Create a token (Note this will open a GitHub browser tab)
+# See steps 6-10 in GitHub's tutorial (link below)
+usethis::create_github_token()
+
+# Copy your Personal Access Token at the end of the above step!
+
+# Now, give your token to RStudio
+# After you run this line you'll follow some prompts in the "Console" tab of RStudio
+gitcreds::gitcreds_set()
+
+usethis::git_sitrep()
