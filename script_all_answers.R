924375c1f245fb4428bf46c0b1532e9e08ab3ab8
Francisco J. Guerrero <guerrero.francisco.jose@gmail.com>
2023-03-13 12:03:10 -0700

    updated color scale bar plot

--- script_all_answers.R
+++ script_all_answers.R
@@ -67,7 +67,7 @@ pq_trigrams %>%
   filter(rank < 20) %>% 
   unite(trigram, word1, word2, word3, sep = " ") %>% 
   ggplot(aes(t_freq, fct_reorder(trigram, t_freq), fill = t_freq)) +
-  scale_fill_gradient(low= "plum", high= "darkorchid4")+
+  scale_fill_gradient(low= "violet", high= "darkorchid4")+
   geom_col(show.legend = FALSE) +
   labs(x = "Frequency", y = NULL)
 
