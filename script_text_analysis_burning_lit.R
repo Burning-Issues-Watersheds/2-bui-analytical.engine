18ca6a783cf4e9e411b0f0a0d455802b76e82221
Francisco J. Guerrero <97982871+guerrero-fj@users.noreply.github.com>
2023-07-08 12:30:45 -0700

    Updated version of network visualization code

--- script_text_analysis_burning_lit.R
+++ script_text_analysis_burning_lit.R
@@ -23,25 +23,33 @@ librarian::shelf(dplyr, tidytext, tidyverse,
 assets_pubs <- "../1-bui-knowledge.base/assets/data/raw" 
 figures <- "../1-bui-knowledge.base/assets/figs" 
 
-t_df <- as_tibble(read_csv(paste(assets_pubs,"survey_answers_zotero_refs.csv",sep='/'),
+references_raw_dat <- as_tibble(read_csv(paste(assets_pubs,"survey_answers_zotero_refs.csv",sep='/'),
                            show_col_types = FALSE))
+head(references_raw_dat)
 
-q_tp <- as_tibble(read_csv(paste(assets_pubs,"question_type_references.csv", sep = '/'),
+question_type_raw_dat <- as_tibble(read_csv(paste(assets_pubs,"question_type_references.csv", sep = '/'),
                            show_col_types = FALSE))
+head(question_type_raw_dat)
+
+################################################################################
+# Data preparation
+###############################################################################
+
+# 1. Data Assembly
 
 # For our analyses below, we won't need DOIs, PMIDs or arXiv IDs. We will focus
 # on Title, Abstract, Authors, Journal, Year. To keep column names formatting
 # simple, I use lowercase and spaces are added with (_) when needed.
 
 # In some cases is possible that the abstracts for the papers are not retrieved. 
-# In such cases,the value returned would be NA (which is read as a character string and not as
-# the typical NA). It could also happen that the abstracts could be partially
-# retrieved. To filter these cases out, we will add another column to the data
-# frame to count the character length of each abstract, and remove those that are
-# less than 20 characters long. Finally, we will add a sequential id and make it
-# a factor for visualization purposes.
+# In such cases,the value returned would be NA (which is read as a character string 
+# and not as the typical NA). It could also happen that the abstracts could be 
+# partially retrieved. To filter these cases out, we will add another column to 
+# the dataframe to count the character length of each abstract, and remove those 
+# that are less than 20 characters long. Finally, we will add a sequential id and 
+# make it a factor for visualization purposes.
 
-t_df_c <- t_df %>% 
+references_dat <- references_raw_dat %>% 
   distinct() %>% 
   select(Key, Title,`Abstract Note`,Author,`Publication Title`,`Publication Year`) %>% 
   rename(key = Key,
@@ -55,33 +63,34 @@ t_df_c <- t_df %>%
   mutate(id = seq(from =1, to= nrow(.),by=1)) %>% 
   mutate(id = factor(id))
 
-t_df_c
+head(references_dat)
 
-# Abstracts per question types:
+# In this case we have abstracts retrieved for all 142 publication items.
 
-abs_q_type <- q_tp %>% 
+# Now, we do some minor reformatting on the `question_type_raw_dat` dataset in 
+# in preparation to be merged with the `reference_dat` dataset.  
+
+question_type_dat <- question_type_raw_dat %>% 
   select(`unique-id`,
          `question-type`,
          key) %>% 
   rename(unique_id = `unique-id`,
-         question_type =`question-type`) %>% 
+         question_type =`question-type`)
+
+# Then, we merge the `question_type_dat`, with the `references_dat` which contains 
+# all the metadata about the publications. In that way, we can subset the entire 
+# reference dataset according to question types.
+
+refereces_question_type_dat <- question_type_dat %>% 
   merge(.,
-        t_df_c,
+        references_dat,
         by = "key",
         all.x = TRUE) %>% 
   filter(is.na(id)==FALSE)
 
+head(refereces_question_type_dat)
 
-# In this case we have abstracts retrieved for all 142 publication items.
-
-# To make sure all words are processed correctly, we need to do some additional 
-# cleaning on the text data. That includes unnesting each publication component 
-# (pub_comp) into tokens (i.e. single words), performing the cleaning tasks 
-# (i.e. singularizing,removing punctuations and digits, removing stop words (a,
-# an, by, ...)), and finally putting the tokens back together (i.e. nesting)
-
-# How to unnest and nest text data in using tidytext? check this post: 
-# https://stackoverflow.com/questions/49118539/opposite-of-unnest-tokens-in-r
+# 2. Data cleaning and tokenization
 
 # Choosing analysis level
 
@@ -91,7 +100,7 @@ abs_q_type <- q_tp %>%
 
 pub_comp = "abstract"
 
-# DATA PREPARATION & TOKENIZATION
+# Tidying text and tokenization
 
 # Our first step to analyze the selected publication component, is to make sure that 
 # our text data is tidy. Tidy text data is all in lower case, singular forms, 
@@ -102,46 +111,66 @@ pub_comp = "abstract"
 # back into "clean" paragprahps.
 
 # There are several packages you could use to tokenize a piece of text, here we 
-# will use the tidytext package for most of our analysis. 
+# will use the `tidytext` package for most of our analysis. 
 
-# What the following chunk of code does is: 
+# How to unnest and nest text data in using tidytext? check this post: 
+# https://stackoverflow.com/questions/49118539/opposite-of-unnest-tokens-in-r
 
 # 1) Define the new data frame, `pub_dat`, as a subset of the `abs_q_type` dataset 
 # and rename the column `abstract` to `pub_comp_words`, so it can be used as a 
-# generic variable in dowstream analysis. 
+# generic variable in downstream analysis:
 
+pub_dat<- dplyr::select(refereces_question_type_dat, 
+                        question_type,
+                        id, 
+                        authors, 
+                        year, 
+                        journal, 
+                        all_of(pub_comp)) %>%
+ rename(pub_comp_words = all_of(pub_comp)) %>% 
+  
 # 2) Break the chunk of (nested) text into tokens (output = word) by using the 
 # function unnest_tokens(). We set the argument `drop` to TRUE so the original 
-# column with the complete abstracts is removed. 
-
+# column with the complete abstracts is removed:
+  
+  unnest_tokens(input = pub_comp_words, 
+                output = word, 
+                drop = TRUE) %>%  
+  
 # 3) We make sure that all of the words are in lower caps and in their singular form
-# with the functions `str_to_lower()` and `singularize()`.
-
-# 4) We remove punctuation, numeric characters, and `stop-words` including articles 
-# and prepositions (e.g., the, a, as, etc.) with `filter()` and `antijoin()`
-
-# 5) We reassemble the abstracts back by nesting (`nest()`) the tokens. This operation
-# stores the tokens as lists associated to each abstract entry. 
-
-# 6) We finally unlist the tokens into tidy paragraphs, which, alike the original 
-# abstract text, are free from special characters, punctuations, etc. 
-
-pub_dat<- dplyr::select(abs_q_type, question_type, id, authors, year, journal, all_of(pub_comp)) %>%
-  rename(pub_comp_words = all_of(pub_comp)) %>% 
-  unnest_tokens(output = word, input = pub_comp_words, drop = TRUE) %>%  
+# with the functions `str_to_lower()` and `singularize()`:
+  
   mutate(word = str_to_lower(word),
          word = singularize(word)) %>% 
+  
+# 4) We remove punctuation, numeric characters, and `stop-words` including articles 
+# and prepositions (e.g., the, a, as, etc.) with `filter()` and `antijoin()`:
+  
   filter(!str_detect(word, "[:punct:]|[:digit:]")) %>% 
   anti_join(stop_words, by = "word") %>% 
+  
+# 5) We reassemble the abstracts back by nesting (`nest()`) the tokens. This operation
+# stores the tokens as lists associated to each abstract entry:
+  
   nest(data = word) %>%  
+  
+# 6) We finally unlist the tokens into tidy paragraphs, which, alike the original 
+# abstract text, are free from special characters, punctuations, etc.:
+  
   mutate(!!paste0(pub_comp) := map_chr(map(data, unlist), paste, collapse = " ")) 
 
+head(pub_dat)
 
-# CONCEPTUAL MAPS FROM N-GRAMS
+################################################################################
+# Conceptual maps from text analysis
+################################################################################
 
+###############################################################################
 # Parameter definitions
 
-# We start defining the minimum number of words (grams) to be linked
+
+# We start defining the minimum number of word-chunks into which the text is going
+# to be broken into (like tidy-words if gram_l = 1 or tidy sentences if gram_l >2.
 gram_l = 4
 
 # We then define the bottom of the frequency ranking to be included in the network
@@ -150,34 +179,51 @@ breath = 250
 # Finally we select the type of question to be represented in the network
 question = "roadblocks"
 
-# Creating the dataset for network analysis
+###############################################################################
+# Creating the dataset for network analysis: 
 
+
+# Defining column names for the dataset
 n_gram <- paste(gram_l,"gram",sep='-')
-
 a <- seq(1:gram_l)
 b <- rep("word",times=gram_l)
 columns <- paste(b,a,sep = '')
 
+# 1) Define the new data frame, `pub_ngrams`, as a subset of the `pub_dat` by 
+# filtering by question type (e.g., pressing, pathways, roadblocks)
+# generic variable in dowstream analysis:
 
 pub_ngrams <- pub_dat %>%
   filter(question_type == question) %>% 
-  ungroup() %>%
+
+# 2) We break the entire text units from `pub_comp` into chunks (`tokens`) of 
+# length = `gram_l` and store the output in the column `n_gram`:
+  
   unnest_tokens(n_gram, pub_comp, token = "ngrams", n = gram_l) %>%
+
+# 3) We now separate the tokens into columns of single words:
+  
   separate(n_gram, columns, sep = " ", remove = FALSE) %>%
+  
+# 4) Finally, we count the frequency of each word across all the columns and 
+# assign each word a rank and calculate its global frequency across all the 
+# abstracts in our sample:
+  
   count(across(all_of(columns), ~.x), sort = TRUE) %>%
   mutate(rank = row_number(),
          total = sum(n),
          t_freq = n/total)
+
 head(pub_ngrams)
+
+###############################################################################
+# Creating network graph
+
 
 # Create the graph using igraph
 ngram_graph <- pub_ngrams %>%
   filter(rank < breath) %>%
   graph_from_data_frame()
-
-
-# Creating the network as an interactive html object to be embedded into the
-# Quarto website
 
 # Create a data frame for nodes
 node_df <- data.frame(id = V(ngram_graph)$name, 
@@ -204,13 +250,17 @@ network <- visNetwork(nodes = node_df, edges = edge_df,
   visNodes(label = "label", title = "title", font = list(size = 20)) %>%
   
   # Customize edges
-  visEdges(arrows = "to") %>%
-  
-  # Add a tooltip
-  visInteraction(hover = TRUE)
+  visEdges(arrows = "to") #%>%
+  # 
+  # # Add a tooltip
+  # visInteraction(hover = TRUE)
 
 # Display the network in the RStudio Viewer pane
 network
+
+###############################################################################
+# Creating the network as an interactive html object to be embedded into the
+# Quarto website
 
 # Generate the HTML code for the network visualization
 html_code <- as.character(tags$div(id = "network-container", network))
@@ -223,13 +273,20 @@ saveWidget(network, file = html_file)
 html_file <- "network.html"
 writeLines(html_code, con = html_file) 
 
+################################################################################
 
 
 
 
 
 
-# Tokenization
+
+
+################################################################################
+# Bonus code
+################################################################################
+# Time-oriented wordclouds
+################################################################################
 
 # Our first step to analyze the selected publication component, is to break it 
 # into individual words (or tokens) and store the tokens in a new data frame for 
